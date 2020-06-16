10ca13495a91ad26a68e63b15cbe29dab36ad52b
langocduong <34342882+langocduong@users.noreply.github.com>
2020-06-16 21:41:56 +0700

    thongtin

---
+++ src/Analysis/thongtin.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+class Thongtin extends Component {
+  
+  render()
+{
+    
+  return (
+    <tr>
+        <td>1</td>
+        <td>Học lập trình</td>
+        <td class="text-center">
+        <span class="label label-success">Kích Hoạt</span>
+         </td>
+        <td class="text-center">
+         <button type="button" class="btn btn-warning">
+        <span class="fa fa-pencil mr-5"></span>Sửa
+        </button>
+        &nbsp;
+        <button type="button" class="btn btn-danger">
+        <span class="fa fa-trash mr-5"></span>Xóa
+        </button>
+        </td>
+        </tr>
+     
+  )
+
+}
+}
+export default Thongtin;
